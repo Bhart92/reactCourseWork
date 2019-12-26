eff4888b3aff64a44c47040b40ffbfec8be4717a
Brandon Hart <Ihartappraisals@gmail.com>
2019-12-26 00:08:00 -0800

    connecting reducers

--- Users/ihart/Documents/projects/react-course-projects/expensify/src/app.js
+++ Users/ihart/Documents/projects/react-course-projects/expensify/src/app.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routers from './routers/Routers';
+import configureStore from './store/configureStore';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
+const store = configureStore();
+
+console.log(store.getState());
 
 
 ReactDOM.render(<Routers />, document.getElementById('app'));
---
+++ Users/ihart/Documents/projects/react-course-projects/expensify/src/selectors/expenses.js
@@ -0,0 +1,9 @@
+export default (expenses, { text, sortBy, startDate, endDate }) => {
+  return expenses.filter((expense) => {
+    const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
+    const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
+    const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
+
+    return startDateMatch && endDateMatch && textMatch;
+  });
+};
