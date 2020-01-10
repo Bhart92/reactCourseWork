47f057c6cf3e6ccfc82e2980770863497250b934
Brandon Hart <Ihartappraisals@gmail.com>
2020-01-10 15:22:31 -0800

    added fixtures folder and tests for selectors, and reducers

---
+++ Users/ihart/Documents/projects/react-course-projects/expensify/src/test/reducers/expenses.test.js
@@ -0,0 +1,64 @@
+import expensesReducer from '../../reducers/expenses';
+import expenses from '../fixtures/expenses';
+
+test('Should set default state', () => {
+	const state = expensesReducer(undefined, {type: '@@INIT'});
+	expect(state).toEqual([]);
+});
+
+test('should remove expense by id', () => {
+  const action = {
+    type: 'REMOVE_EXPENSE',
+    id: expenses[1].id
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual([expenses[0], expenses[2]]);
+});
+
+test('Should not remove expenses if id is not found', () => {
+	const action = {
+	type: 'REMOVE_ EXPENSE',
+	id: -1
+	};
+	const state = expensesReducer(expenses, action);
+	expect(state).toEqual(expenses);
+});
+test('should add an expense', () => {
+	const expense = {
+	id: '109',
+	description: 'laptop',
+	note: undefined,
+	createdAt: 20000,
+	amount: 295
+	};
+	const action = {
+	type: 'ADD_EXPENSE',
+	expense
+	};
+	const state = expensesReducer(expenses, action);
+	expect(state).toEqual([...expenses, expense]);
+});
+test('should edit an expense', () => {
+	const amount = 1220;
+	const action = {
+		type: 'EDIT_EXPENSE',
+		id: expenses[1].id,
+		updates: {
+			amount
+		}
+  };
+	const state = expensesReducer(expenses, action);
+	expect(state[1].amount).toBe(amount);
+});
+test('should not edit an expense if not found', () => {
+	const amount = 1220;
+	const action = {
+		type: 'EDIT_EXPENSE',
+		id: -1,
+		updates: {
+			amount
+		}
+  };
+	const state = expensesReducer(expenses, action);
+	expect(state).toEqual(expenses);
+});
