0123f91c209a0ec532399f8933de3e5896f22977
Brandon Hart <Ihartappraisals@gmail.com>
2020-04-13 08:23:55 -0700

    created PrivateRoute.js

--- Users/ihart/Documents/projects/react-course-projects/MERN/client/src/components/auth/Register.js
+++ Users/ihart/Documents/projects/react-course-projects/MERN/client/src/components/auth/Register.js
@@ -1,11 +1,12 @@
 import React, { Fragment, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import {connect} from 'react-redux';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
 
-const Register = (props) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
     const [ formData, setFormData ] = useState({
         name: '',
         email: '',
@@ -17,11 +18,16 @@ const Register = (props) => {
     const onSubmit = e => {
         e.preventDefault();
         if(password !== password2){
-            props.setAlert('Passwords do not match', 'danger');
+            setAlert('Passwords do not match', 'danger');
         } else{
-            console.log(formData);
+            register({ name, email, password });
         }
     };
+
+     //Redirect if logged in
+    if(isAuthenticated){
+        return <Redirect to="/dashboard" />
+    }
     return <Fragment>
         <h1 className="large text-primary">Sign Up</h1>
         <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
@@ -76,7 +82,12 @@ const Register = (props) => {
 };
 
 Register.propTypes = {
-    setAlert: PropTypes.func.isRequired
+    setAlert: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool,
 };
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+});
 
-export default connect(null, { setAlert })(Register);
+export default connect(mapStateToProps, { setAlert, register })(Register);
---
+++ Users/ihart/Documents/projects/react-course-projects/MERN/client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Dashboard = props => {
+    return (
+        <div>
+            Dashboard
+        </div>
+    )
+};
+
+Dashboard.propTypes = {
+
+};
+
+export default Dashboard;
